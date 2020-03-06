992e12ea366f415a0278c2865cf125ede69b842b
kumaresh easwaran <kuma@kumareshs-MacBook-Pro.local>
2020-03-06 16:24:00 -0500

    intial commit

---
+++ API/Todo.Api/Models/TodoItem.cs
@@ -0,0 +1,8 @@
+namespace Todo.Api.Models
+{
+    public class TodoItem{
+        public int Id{ get; set;}
+        public string Action{ get; set;}
+        public bool IsDone {get; set;}
+    }
+}
